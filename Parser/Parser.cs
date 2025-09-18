e57818d6120f6be729a97856ed8212f1b00abfb8
Anders1733 <anders1733@mail.ru>
2025-09-18 20:44:37 +0300

    Добавил метод вывода значения конкретной ячейки

---
+++ Parser/Parser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+
+namespace ExcelEaterConsoleEdition.Parser
+{
+    public class Parser
+    {
+        public static string ReadCellValue(string filePath, int sheetIndex, int rowNumber, int columnNumber)
+        {
+            ExcelPackage.License.SetNonCommercialOrganization("ABOBA");
+            using var package = new ExcelPackage(new FileInfo(filePath));
+
+            Console.WriteLine(package.Workbook.Worksheets.Count);
+            
+
+            var worksheet = package.Workbook.Worksheets[sheetIndex];
+            Console.WriteLine();
+            foreach (var worksheet1 in package.Workbook.Worksheets)
+            {
+                Console.WriteLine("Name = " + worksheet1.Name);
+                Console.WriteLine("Index = " + worksheet1.Index);
+                Console.WriteLine("MaxRow = " + worksheet.Dimension.End.Row);
+                Console.WriteLine("MaxColumns = " + worksheet.Dimension.End.Column);
+            }
+            
+            // Проверяем наличие строки и столбца перед чтением
+            if (worksheet.Dimension.End.Row >= rowNumber && worksheet.Dimension.End.Column >= columnNumber)
+            {
+                return worksheet.Cells[rowNumber, columnNumber].Text;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Указанная ячейка выходит за пределы диапазона листа.");
+            }
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExcelEaterConsoleEdition.Parser;
+
+const string FILE_PATH = "C:\\Projects\\ExcelEaterConsoleEdition\\PlateForExcelFiles\\test.xlsl";
+
+var cellValue = Parser.ReadCellValue("test.xlsx", 0, 2, 5);
+
+Console.WriteLine(cellValue);
